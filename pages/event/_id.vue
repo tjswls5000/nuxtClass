9a2b9f8a2c35a2515af30f87c9dda1aa7c351b26
Sunjin Baek <tjswls5000@gmail.com>
2020-04-23 01:11:31 +0900

    7장까지 완료

---
+++ pages/event/_id.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>{{ event.title }}</h1>
+  </div>
+</template>
+
+<script>
+import EventService from '@/services/EventService.js'
+
+export default {
+  async asyncData({ error, params }) {
+    try {
+      const { data } = await EventService.getEvent(params.id)
+      return {
+        event: data
+      }
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch event #' + params.id
+      })
+    }
+  },
+  head() {
+    return {
+      title: this.event.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'What you need to know about title' + this.event.title
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/event/create.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Create Events</h1>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create an Event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'You can create a new event in your neighborhood'
+        }
+      ]
+    }
+  }
+}
+</script>
